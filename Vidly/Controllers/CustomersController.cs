5650e071827e5b442e14420a9a99d940d3c19da9
DESKTOP-58C525U\Maria <irimescu.mariaalexandra@yahoo.com>
2018-03-19 09:51:49 +0200

    Exercise: Section 2, lecture 19

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer{Id = 1, Name="Andreea Catalina"},
+                new Customer{Id = 2, Name="Alexandra Maria"}
+            };
+
+            var viewModel = new CustomersViewModels
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        //[Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer{Id = 1, Name="Andreea Catalina"},
+                new Customer{Id = 2, Name="Alexandra Maria"}
+            };
+
+           
+            var result = customers.Find(x => x.Id == id);
+
+            if(result == null)
+            {
+                return HttpNotFound();
+            }
+            return View(result);
+        }
+    }
+}
