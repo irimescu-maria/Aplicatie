88aeaeaccb57996b3b99558ce75bfbd790bfadc2
DESKTOP-58C525U\Maria <irimescu.mariaalexandra@yahoo.com>
2018-03-23 21:35:05 +0200

    Exercise 6: Section 6

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
+        [Range(1, 20)]
+        //[RangeNumberInStock]
+        public byte NumberInStock { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+    }
+}
